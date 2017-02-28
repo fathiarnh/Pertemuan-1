4048f9a52dee94c440f80620656a0f0ef5edb24a
Fathia Rahma Nur Hanifa <rahma.hanifa20@gmail.com>
2017-02-28 19:49:34 +0700

    Add files via upload

---
+++ Pertemuan 2/Mahasiswa.java
@@ -0,0 +1,11 @@
+class Mahasiswa{
+	public static void main(String[] args) {
+		System.out.println("\n==============================================");
+		System.out.println("\t\tBIODATA");
+		System.out.println("==============================================");
+		System.out.println("NIM\t: 10115405");
+		System.out.println("NAMA\t: Fathia Rahma Nur Hanifa");
+		System.out.println("ALAMAT\t: Jl. Ibu Ganirah No.41 Cimahi Selatan");
+		System.out.println("==============================================");
+	}
+}
